e17a5bd0a946b86ca61c237ad15b7f1eccd5c6af
Rasaal Ahmad <56187211+rasaalahmad@users.noreply.github.com>
2023-05-09 14:44:42 -0700

    Initial Commit

---
+++ CollecionView Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollecionView Sample
+//
+//  Created by Rasaal Ahmad on 09/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
